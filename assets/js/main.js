d52747c09102582a3285bb01434838aff13c89d7
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-04-20 12:35:56 -0300

    feat: start to fetch list of pokemons from api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("Sucesso!");
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.results))
+    .then((pokemonList) => {
+        console.log(pokemonList)
+    })
+    .catch((error) => console.log(error));
