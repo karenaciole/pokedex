ceb26037a64aa05140b55ce10b8c746f541afd7b
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-24 18:59:09 -0300

    feat: add bg colors of pokemon type

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,13 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 const convertPokemonToList = (pokemon) => {
     return `
-        <li class="pokemon" ${pokemon.primaryType}>
+        <li class="pokemon">
             <span class="id">#${pokemon.id}</span>
             <span class="name">${pokemon.name}</span>
             
             <div class="detail">
                 <ol class="types">
-                   ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                   ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
                 
                 <img src="${pokemon.image}" alt="${pokemon.name}">
